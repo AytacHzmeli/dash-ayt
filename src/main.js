2bef6c28cb29bfbb9d8fa8c321212fa2d1dd6d78
Aytaç Hüzmeli <a.hzmeli@hotmail.com>
2022-08-24 13:31:03 +0300

    proje

--- src/main.js
+++ src/main.js
@@ -2,9 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.css";
+import "./assets/scss/material-dashboard.scss";
+import SideBar from "./components/SidebarPlugin";
+
 Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+export default {
+  install(Vue) {
+    Vue.use(SideBar);
+    Vue.use(VueMaterial);
+  },
+};
--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,52 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import DashboardLayout from '@/pages/Layout/DashboardLayout.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import DashboardLayout from "@/pages/Layout/DashboardLayout.vue";
 import HomeView from "@/pages/HomeView.vue";
 import DashBoard from "@/pages/DashBoard.vue";
+import UserProfile from "@/pages/UserProfile.vue";
+import Icons from "@/pages/Icons.vue";
+import TableList from "@/pages/TableList.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'homeview',
-    component: HomeView
+    path: "/",
+    name: "homeview",
+    component: HomeView,
   },
   {
-      path: 'DashboardLayout',
-      name: 'DashboardLayout',
-      component: DashboardLayout
+    path: "DashboardLayout",
+    name: "DashboardLayout",
+    component: DashboardLayout,
   },
   {
     path: "dashboard",
     name: "DASHBOARD",
     component: DashBoard,
-},
-]
+  },
+  {
+    path: "user",
+    name: "USERPROFILE",
+    component: UserProfile,
+  },
+  {
+    path: "icons",
+    name: "ICONS",
+    component: Icons,
+  },
+  {
+    path: "table",
+    name: "Table List",
+    component: TableList,
+  },
+  
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
