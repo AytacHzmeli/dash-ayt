c3bb69e01e9b659da31bbf52e85befcd9fe02dc1
Aytaç Hüzmeli <a.hzmeli@hotmail.com>
2022-08-24 13:30:08 +0300

    projeolusturuldu

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashboardLayout from '@/pages/Layout/DashboardLayout.vue'
+import HomeView from "@/pages/HomeView.vue";
+import DashBoard from "@/pages/DashBoard.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'homeview',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+      path: 'DashboardLayout',
+      name: 'DashboardLayout',
+      component: DashboardLayout
+  },
+  {
+    path: "dashboard",
+    name: "DASHBOARD",
+    component: DashBoard,
+},
 ]
 
 const router = new VueRouter({
